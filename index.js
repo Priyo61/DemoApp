f23a0c9dc5dc19f44ee05c22082ffbd34ad03726
Priyobrata Sorokhaibam <priyobratasorokhaibam@Priyobratas-MacBook-Air.local>
2025-06-05 06:25:10 +0530

    add

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
-app.get("/", (req, res) => {
+app.post("/user/signup", (req, res) => {
   res.json({
-    mgs: "home page",
+    mgs: "sign up page",
   });
 });
-app.post("/", (req, res) => {
+app.post("/user/signin", (req, res) => {
   res.json({
-    mgs: "home page",
+    mgs: "sign in page",
+  });
+});
+app.get("/user/purchases", (req, res) => {
+  res.json({
+    mgs: "sign in page",
+  });
+});
+
+app.post("/course/purchase", (req, res) => {
+  res.json({
+    mgs: "sign in page",
+  });
+});
+
+app.get("/courses", (req, res) => {
+  res.json({
+    mgs: "endpind",
   });
 });
 
