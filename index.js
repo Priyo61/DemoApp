4d4ab435514b21e135dba66666a0f74ef0262be1
Priyobrata Sorokhaibam <priyobratasorokhaibam@Priyobratas-MacBook-Air.local>
2025-06-05 07:18:13 +0530

    add version1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 
-app.use("/user", userRouter);
-app.use("/course", courseRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
 
 app.listen(3030);
